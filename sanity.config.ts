83c4a630500f39af994a1cca379982a8cc89aa5d
Vincent Laurensius <vincenthambaya@gmail.com>
2023-02-20 11:16:58 +0700

    [FEAT] Enhance preview mode with live preview sites in studio slug

--- sanity.config.ts
+++ sanity.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "sanity";
 import { deskTool } from "sanity/desk";
 import { visionTool } from "@sanity/vision";
 import { schemaTypes } from "./schemas";
+import { getDefaultDocumentNode } from "./structure";
 
 const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!;
 const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!;
@@ -12,7 +13,7 @@ export default defineConfig({
   title: "ARCK Content Studio",
   projectId,
   dataset,
-  plugins: [deskTool(), visionTool()],
+  plugins: [deskTool({defaultDocumentNode: getDefaultDocumentNode }), visionTool()],
   schema: {
     types: schemaTypes,
   },
---
+++ structure.ts
@@ -0,0 +1,27 @@
+import { DefaultDocumentNodeResolver } from "sanity/desk";
+import Iframe from "sanity-plugin-iframe-pane";
+
+export const getDefaultDocumentNode: DefaultDocumentNodeResolver = (
+  S,
+  { schemaType }
+) => {
+  // Only show preview pane on `post` schema type documents
+  if (schemaType === "post") {
+    return S.document().views([
+      S.view.form(),
+      S.view
+        .component(Iframe)
+        .options({
+          url: `${
+            process.env.NEXT_PUBLIC_VERCEL_URL || "https://localhost:3000"
+          }/api/preview`,
+          defaultSize: "desktop",
+          reload: {
+            button: true,
+          },
+          attributes: {},
+        })
+        .title("Preview"),
+    ]);
+  }
+};
